7018eef9880134fdbe6c132cefbe18bd8133ae0d
Reinejess <lore.jessmedina@gmail.com>
2022-04-24 12:10:06 -0500

    fixing dates and calender

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
 var tasks = {};
+var auditTask = function(taskEl) {
+  //get data from task element
+  var date = $(taskEl)
+    .find("span")
+    .text()
+    .trim();
+
+    //convert to moment object at 5:00pm
+    var time = moment(date, "L").set("hour", 17);
+
+    //remove any old classes from elemeent 
+    $taskEl.removeClass("list-group-item-warning list-group-item-danger");
+
+    //apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+      $(taskEl).addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <=2) {
+      $(taskEl).addClass("list-group-item-warning");
+    }
+};
 
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +36,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -133,7 +158,7 @@ $(".list-group").on("blur", "textarea", function() {
       .trim();
 
     //create new input element
-    var dataInput = $("<input")
+    var dataInput = $("<input>")
       .attr("type", "text")
       .addClass("form-control")
       .val(date);
@@ -141,12 +166,21 @@ $(".list-group").on("blur", "textarea", function() {
     //swap out elements
     $(this).replaceWith(dateInput);
 
-    //automatically focus on new element
+    //enable jquery ui datepicker
+    dateInput.datepicker({
+      minDate: 1,
+      onClose: function() {
+        //when calender is closed, force a "change" event on the 'dateInput'
+        $(this).trigger("change");
+      }
+    });
+
+    //automatically bring up the calender
     dateInput.trigger("focus");
   });
 
 //value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   //get current text
   var date = $(this)
     .val()
@@ -174,6 +208,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
   
   //replace input with span element
     $(this).replaceWith(taskSpan);
+
+    //pass task's <li> element into auditTask() to check new date
+    auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 $(".card .list-group").sortable({
@@ -250,6 +287,12 @@ $("#trash").droppable({
 
   
 });
+
+
+$("#modalDueDate").datepicker( {
+  minDate: 1
+});
+
 
 
 // remove all tasks
